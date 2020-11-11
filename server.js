fbbeaeaf27944e768ff1a93cba4909b7e77c769f
Niki Fereidooni <nikifereidooni@gmail.com>
2020-11-10 23:48:51 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require ('express')
+const exphbs = require('express-handlebars')
+const path = require('path');
+const fs = require('fs');
 
+const PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use( express.static('public') )
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(PORT, function () {
+    console.log("Serving burgers on PORT " + PORT);
+    
+});
