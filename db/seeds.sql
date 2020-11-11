46af59921e48aaa44526b18c5f9818b57f9c5de6
Niki Fereidooni <nikifereidooni@gmail.com>
2020-11-10 23:55:09 -0500

    added schema and seeds data

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Portabello & Swiss Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Avocado Bacon Ranch Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Teriyaki Salmon Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Beyond Meat Burger");
