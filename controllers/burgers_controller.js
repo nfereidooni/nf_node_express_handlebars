24ace4201ddb0d58401f1fc27e847c7106c60370
Niki Fereidooni <nikifereidooni@gmail.com>
2020-11-11 20:42:16 -0500

    updated controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,26 +2,22 @@ const express = require("express");
 const burger = require("../models/burger.js");
 const router = express.Router();
 
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-        const hbObject = {
-            burgers: data
-        };
-        res.render('index', hbObject);
-    });
+router.get("/", async function(req, res) {
+    const result = await burger.showBurgers();
+    res.render("index", { burgers: result })
 });
 
-
-router.post("/", function(req, res) {
-
-// enter code
-
+router.post("/", async function(req, res) {
+    burgerName = req.body.burger_name
+    const result = await burger.submitBurger(burgerName)
+    res.send({result})
 });
 
-router.put("/:id", function(req, res) {
-
-// enter code
-
+router.put("/:id", async function(req, res) {
+    const id = req.params.id
+    const devoured = req.body.devoured
+    const result = await burger.devourBurger(id, devoured)
+    res.json(result)
 });
 
 module.exports = router;
